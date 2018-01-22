8df4e217fa796b306b9a201c9d5f39d5ac5eb068
SArseneault <madslavegarden@gmail.com>
2018-01-22 12:17:59 -0500

    moved files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int runCount = 1;
+            while(true){
+                Console.WriteLine("Test Run#: " + runCount);
+                runCount += 1;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
