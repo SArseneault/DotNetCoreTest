2ad6fd46928b454986294792403681446ce89195
SArseneault <madslavegarden@gmail.com>
2018-02-18 19:23:37 -0500

    Removed echo lines

--- kube manifest files/init.sh
+++ kube manifest files/init.sh
@@ -1,8 +1,5 @@
 #!/user/bin/env bash
 
-echo 'Creating application deployment'
 kubectl create -f app-Deployment.yaml
-
-echo 'Creating service to expose applications port'
 kubectl create -f ingress-service.yaml
 
